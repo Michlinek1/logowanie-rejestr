e04c6fa7493afba53951a29892bf1bdaae9db0b2
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-04-27 19:32:04 +0200

    Update ustawienia.php

--- ustawienia.php
+++ ustawienia.php
@@ -5,47 +5,57 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ustawienia</title>
+    <link rel="stylesheet" href="style.php" media="screen">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
 <?php
 session_start();
-$pol = new mysqli("localhost", "root", "", "baza");
-$nick = $_SESSION["nick"];
-$haslo = $_SESSION["haslo2"];
-$result=mysqli_query($pol, "SELECT * FROM dane WHERE login = '$nick' and haslo2 = '$haslo'");
-if ($result->num_rows>0) {
-    while ($wiersz = $result->fetch_assoc()) {
-        echo "Nick: ".$wiersz["login"]."<br>";
-        echo "Hasło: ".$wiersz["haslo"]."<br>";
-    }
-}else{
-    echo 'blad';
-}
-
-$przycisk = $_POST['Przycisk'];
-
-if(isset($przycisk)){
-    if(isset($_POST["inputhaslo"]) && $_POST["inputhaslo"]!=""){
-        $haslo = $_POST["inputhaslo"];
-        $sql = "UPDATE dane SET haslo2 = '$haslo' WHERE login = '$nick'";
-        $result = mysqli_query($pol, $sql);
-        if($result){
-           header("location:index.php");
-        }
-        else{
-            echo "Hasło nie zostało zmienione";
-        }
-    }
-}
-
-
 
 ?>
+  <nav>
+    <ul>
+       <li><a class ="pod" href="index.php" >Strona glowna</a></li>
+       <li><a class ="active" href="#" >Ustawienia</a></li>
+    </ul>
+  </nav>
+  <table>
+      <tr>
+          <th >Twój Login </th>
+          <th>Twoje hasło </th> 
+      </tr>
+      <tr>
+            <td><?php echo $_SESSION['nick']; ?></td>
+            <td><?php echo $_SESSION['haslo']; ?></td>
+      </tr>
+  </table>
 <form action="" method="POST">
-<input type = "text" name = "inputhaslo" placeholder="Nowe hasło">
-<button type="submit" id="przycisk" name="Przycisk">Zmień hasło</button>
+<input type = "text" name = "zmianalogin" placeholder="Zmień hasło">
+<button type="submit" id="Przycisk" name="Przycisk">Zmień hasło</button>
+
 </form>
+<?php
+if(isset($_POST['Przycisk'])){
+    if($_POST['zmianalogin']!="" && $_SESSION['haslo']!=$_POST['zmianalogin']){
+        $pol = new mysqli("localhost", "root", "", "baza");
+        $result=mysqli_query($pol, "SELECT * FROM dane WHERE login = '$_SESSION[nick]'");
+        if ($result->num_rows>0) {
+            while ($wiersz = $result->fetch_assoc()) {
+                if($_SESSION["nick"]==$wiersz["login"]){
+                    $pol->query("UPDATE dane SET haslo2 = '$_POST[zmianalogin]' WHERE login = '$_SESSION[nick]'");
+                    $_SESSION['haslo'] = $_POST['zmianalogin'];
+                    echo "<h2 class='tekst'>Zmieniłeś hasło pomyślnie </h1>";  
+                    header("Refresh:0");
+
+                }
+            }
+        }
+    }else{
+        echo "<h2 class='tekst'>Podaj nowe hasło </h1>";
+    }
+}
+
+?>
 
 </body>
 </html>

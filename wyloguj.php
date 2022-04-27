addd6107daafa64b1fc37dbd9dbb2571827dbb11
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-04-27 19:32:22 +0200

    Update wyloguj.php

--- wyloguj.php
+++ wyloguj.php
@@ -1,1 +1,23 @@
-c
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wyloguj sie</title>
+</head>
+<body>
+    <?php
+    session_Start();
+
+    if ($_SESSION['zalogowany'] == true) {
+        $_SESSION['zalogowany'] = false;
+        header('Location: index.php');
+    }
+    
+
+
+    ?>
+    
+</body>
+</html>

8bf1e6b2616d65d022ef20f45291ba8820085bd9
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-04-27 19:32:38 +0200

    Add files via upload

---
+++ style.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-type: text/css");
+?>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,300&family=Roboto&family=Square+Peg&display=swap');
+.tekst{
+    text-align:center;
+    font-family: 'Roboto';
+}

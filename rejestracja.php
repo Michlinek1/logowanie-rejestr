bf7ffc8e3e9a38218d379202ae7b5ed03346e4a6
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-04-27 19:31:52 +0200

    Update rejestracja.php

--- rejestracja.php
+++ rejestracja.php
@@ -1,3 +1,8 @@
+<?php
+error_reporting(0);
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,19 +10,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rejestracja</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.php" media="screen">
     <link rel="stylesheet" href="style.css">
+    
 </head>
 <body id = "Rejestracja">
 <nav>
     <ul>
-       <a class ="pod" href="index.php" ><li>Strona glowna</li></a>
-       <a class ="pod" href="logowanie.php" ><li>Logowanie</li></a>
-       <a class ="pod" href="rejestracja.php" ><li>Rejestracja</li></a>
-    </ul>
-    </nav>
+       <li><a class ="pod" href="index.php" >Strona glowna</a></li>
+       <li><a class ="pod" href="logowanie.php" >Logowanie</a></li>
+       <li><a class ="active" href="rejestracja.php" >Rejestracja</a></li>
+    <?php
+
+if($_SESSION['zalogowany']==true){
+    echo "<li><a class = 'pod' href = 'ustawienia.php'>Ustawienia</li></div>";
+    echo "<li><a class = 'pod' href='wyloguj.php'>Wyloguj</a></li>";
+    echo "</ul>";
+    echo " </nav>";
+}
+else{   
+    echo "</ul>";
+    echo " </nav>";
+}
+
+?>
     <form method="post">
         <div class="form-group">
             <input type="text" class="form-control" id="login" name="login" placeholder="Wpisz login"   autocomplete="off" > <br><br>
@@ -28,11 +44,10 @@
         <div class="form-group">
             <input type="password" class="form-control" id="password2" name="password2" placeholder="Powtórz hasło"><br><br>
         </div>
-        <button type="submit" id="przycisk" name="Przycisk">Zarejestruj</button>
+        <button type="submit" id="Przycisk" name="Przycisk">Zarejestruj</button>
 </form>
     <?php
     error_reporting(1);
-    session_start();
     $pol = new mysqli("localhost", "root", "", "baza");
     if (mysqli_connect_error()) {
       die("Nie Połączono" . mysqli_connect_error())."<br>";
@@ -54,10 +69,14 @@
         if(isset($przycisk)){
             if(!empty($login) && !empty($haslo) && !empty($haslo2)){
             if($haslo == $haslo2){
+                if($login == "admin" || $haslo = "admin"){
+                    echo "<h2 class='tekst'>Nie możesz użyć hasła lub loginu admin</h1>";
+                }
+
                 
                 while($row = mysqli_fetch_array($sqlsprawdzanie)){
                     if($row['login'] == $login){
-                        echo "Login jest już zajęty";
+                        echo "<h1 class='tekst'>Login jest już zajęty </h1>";
                         break;
                     }
                 }
@@ -70,14 +89,15 @@
                 }
             }
             else{
-                echo "Hasła nie są takie same";
+                echo "<h2 class='tekst'>Hasla nie są takie same! </h2>";
             }
             }
             else{
-            echo "Wypełnij wszystkie pola";
+            echo "<h2 class='tekst'>Wypełnij wszystkie pola </h1>";
             }
+            
         }
-
+      
 ?>
     
 </body>
